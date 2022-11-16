2b3cc97a4bc3b7ecd2446e94b1437d1b2257e7da
Víctor Otero Cabaleiro <45633301+Wildrou@users.noreply.github.com>
2022-11-16 17:01:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# response body to json and normalize from tree to table.
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display dataframe
+streamlit.dataframe(fruityvice_normalized)
