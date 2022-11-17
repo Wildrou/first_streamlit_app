8ce3eaa69ec3e6ab80ae96fb3eb053b65e465be7
Víctor Otero Cabaleiro <45633301+Wildrou@users.noreply.github.com>
2022-11-17 14:53:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import snowflake.connector
 import requests
-from urllib.error import URLERROR
+from urllib.error import URLError
 
 streamlit.title("My parents new healthy diner")
 
